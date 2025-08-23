2a29f76cbf0cdea6831e409c07de7b4e29388372
sweet.dh <dlsrksrhk@gmail.com>
2025-08-23 18:45:07 +0900

    메타데이터, 레이아웃

--- next-app1/app/@users/page.tsx
+++ next-app1/app/@users/page.tsx
@@ -1,3 +1,26 @@
+import {Metadata} from "next";
+
+// export const metadata: Metadata = {
+//     title: '유저 페이지',
+//     description: '유저 페이지입니다.',
+// }
+
+
+export async function generateMetadata(
+    {
+        searchParams,
+    }: {
+        searchParams: Promise<{ q?: string; }>;
+    }): Promise<Metadata> {
+    const param = await searchParams;
+
+    return {
+        title: `${param.q} 상세 페이지`,
+        description: `${param.q} 상세 페이지 결과입니다.`,
+    };
+}
+
+
 function UserPage() {
     return (
         <div className='box slot-2'>
